775b5b88f8ed05a704688b423b5fd5e2db0219c5
Fahim <fahimfahim0407@gmail.com>
2026-05-15 18:50:19 +0700

    db: Add realistic geopolitical relations for South America

---
+++ postgre/db_hubungan_geopolitik/amerika_selatan/190_chile.sql
@@ -0,0 +1,18 @@
+-- Hubungan Geopolitik untuk chile (REALISTIC DATA)
+INSERT INTO hubungan_negara (country_id, target_country_id, relation_value) VALUES
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Afrika Selatan' OR name_id = 'Afrika Selatan' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Albania' OR name_id = 'Albania' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Andorra' OR name_id = 'Andorra' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Angola' OR name_id = 'Angola' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bahrain' OR name_id = 'Bahrain' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bangladesh' OR name_id = 'Bangladesh' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Belarus' OR name_id = 'Belarus' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Belgia' OR name_id = 'Belgia' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Belize' OR name_id = 'Belize' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Benin' OR name_id = 'Benin' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bermuda' OR name_id = 'Bermuda' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bhutan' OR name_id = 'Bhutan' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bolivia' OR name_id = 'Bolivia' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bosnia dan Hercegovina' OR name_id = 'Bosnia dan Hercegovina' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Botswana' OR name_id = 'Botswana' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'chile' OR name_id = 'chile' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Brunei' OR name_id = 'Brunei' LIMIT 1), 50);
---
+++ postgre/db_hubungan_geopolitik/amerika_selatan/192_paraguay.sql
@@ -0,0 +1,13 @@
+-- Hubungan Geopolitik untuk paraguay (REALISTIC DATA)
+INSERT INTO hubungan_negara (country_id, target_country_id, relation_value) VALUES
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Angola' OR name_id = 'Angola' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Antigua dan Barbuda' OR name_id = 'Antigua dan Barbuda' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Argentina' OR name_id = 'Argentina' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Armenia' OR name_id = 'Armenia' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Australia' OR name_id = 'Australia' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bahama' OR name_id = 'Bahama' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bangladesh' OR name_id = 'Bangladesh' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Belize' OR name_id = 'Belize' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Benin' OR name_id = 'Benin' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bermuda' OR name_id = 'Bermuda' LIMIT 1), 50),
+  ((SELECT id FROM countries WHERE name_en = 'paraguay' OR name_id = 'paraguay' LIMIT 1), (SELECT id FROM countries WHERE name_en = 'Bolivia' OR name_id = 'Bolivia' LIMIT 1), 50);
